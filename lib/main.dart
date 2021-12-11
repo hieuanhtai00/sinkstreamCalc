3b31e3a05cedb679f3cd87d18088e7121a70be33
Nguyễn Văn Hiếu <90753665+hieuanhtai00@users.noreply.github.com>
2021-12-11 15:41:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,157 +29,88 @@ class MyApp extends StatelessWidget {
 class MyCalculator extends StatelessWidget {
   Calculator calculatorController = Calculator();
   MyCalculator({Key? key}) : super(key: key);
+  Widget button({required void onpress, required Widget child}) {
+    return TextButton(
+      onPressed: () {
+        onpress;
+      },
+      child: child,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-          children: [
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Colors.black,
-                ),
-                child: Center(
-                  child: StreamBuilder(
-                    stream: calculatorController.calculatorStream,
-                    builder: (context, output) {
-                      return Text(
-                        output.hasData ? output.data.toString() : '',
-                        style: TextStyle(fontSize: 30, color: Colors.white),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              flex: 2,
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Colors.black,
             ),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 4,
-                mainAxisSpacing: 4,
-                crossAxisSpacing: 4,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.cancel();
-                    },
-                    child: butC,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.cancel();
-                    },
-                    child: butpercent,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.cancel();
-                    },
-                    child: but$,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputOperator('/');
-                    },
-                    child: butdiv,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('7');
-                    },
-                    child: but7,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('8');
-                    },
-                    child: but8,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('9');
-                    },
-                    child: but9,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputOperator('-');
-                    },
-                    child: butsub,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('4');
-                    },
-                    child: but4,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('5');
-                    },
-                    child: but5,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('6');
-                    },
-                    child: but6,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputOperator('+');
-                    },
-                    child: butsum,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('1');
-                    },
-                    child: but1,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('2');
-                    },
-                    child: but2,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('3');
-                    },
-                    child: but3,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputOperator('*');
-                    },
-                    child: butmul,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.inputNumber('0');
-                    },
-                    child: but0,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.cancel();
-                    },
-                    child: butdot,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      calculatorController.calc();
-                    },
-                    child: butequal,
-                  ),
-                ],
+            child: Center(
+              child: StreamBuilder(
+                stream: calculatorController.calculatorStream,
+                builder: (context, output) {
+                  return Text(
+                    output.hasData ? output.data.toString() : '',
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  );
+                },
               ),
-              flex: 8,
             ),
-          ],
-      );
+          ),
+          flex: 2,
+        ),
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 4,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            children: [
+              button(onpress: calculatorController.cancel(), child: butC),
+              button(onpress: calculatorController.cancel(), child: butpercent),
+              button(onpress: calculatorController.cancel(), child: but$),
+              button(
+                  onpress: calculatorController.inputOperator('/'),
+                  child: butdiv),
+              button(
+                  onpress: calculatorController.inputNumber('7'), child: but7),
+              button(
+                  onpress: calculatorController.inputNumber('8'), child: but8),
+              button(
+                  onpress: calculatorController.inputNumber('9'), child: but9),
+              button(
+                  onpress: calculatorController.inputOperator('-'),
+                  child: butsub),
+              button(
+                  onpress: calculatorController.inputNumber('4'), child: but4),
+              button(
+                  onpress: calculatorController.inputNumber('5'), child: but5),
+              button(
+                  onpress: calculatorController.inputNumber('6'), child: but6),
+              button(
+                  onpress: calculatorController.inputOperator('+'),
+                  child: butsum),
+              button(
+                  onpress: calculatorController.inputNumber('1'), child: but1),
+              button(
+                  onpress: calculatorController.inputNumber('2'), child: but2),
+              button(
+                  onpress: calculatorController.inputNumber('3'), child: but3),
+              button(
+                  onpress: calculatorController.inputOperator('*'),
+                  child: butmul),
+              button(
+                  onpress: calculatorController.inputNumber('0'), child: but0),
+              button(onpress: calculatorController.cancel(), child: butdot),
+              button(onpress: calculatorController.calc(), child: butequal),
+            ],
+          ),
+          flex: 8,
+        ),
+      ],
+    );
   }
 }
